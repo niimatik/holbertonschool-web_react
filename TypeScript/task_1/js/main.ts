3e7e1c5ed872f55b513fc014196a85dba4dc2b86
niimatik <niimatik.gg@gmail.com>
2026-01-16 14:17:49 +0100

    add: completed the task 4

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -6,18 +6,44 @@ interface teacher {
 	yearsOfExperience?: number ;
 	location: string;
 	[key: string]: any;
-}
+};
 
 // Task 2
 interface Directors extends teacher {
 	numberOfReports: number
-}
+};
 
 // Task 3
-interface printTeacherFunction{
+interface printTeacherFunction {
 	(firstName: string, lastName: string): string
-}
+};
 
 const printTeacher: printTeacherFunction = (firstName, lastName) => {
 	return `${firstName.charAt(0)}. ${lastName}`;
-}
+};
+
+// task 4
+interface StudentClassInterface {
+	workOnHomework(): string;
+	displayName(): string
+};
+
+interface studentClassConstructor {
+	new (firstName: string, lastName: string): string
+};
+
+class studentClass implements StudentClassInterface {
+	constructor(public firstName: string, public lastName: string) {}
+
+	workOnHomework(): string {
+		return "Currently working";
+	}
+
+	displayName(): string {
+		return this.firstName;
+	}
+};
+
+const studentobj = new studentClass("matis", "Neveu");
+console.log(studentobj.workOnHomework());
+console.log(studentobj.displayName());

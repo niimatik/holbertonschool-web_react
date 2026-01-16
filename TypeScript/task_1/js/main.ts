a25e0a725043c791faf3afe8f91c7ee375f7ae23
niimatik <niimatik.gg@gmail.com>
2026-01-16 10:22:55 +0100

    add: finish the task 1

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -1,29 +1,18 @@
-interface student {
+interface teacher {
 	firstName: string;
 	lastName: string;
-	age: number;
-	location: string
+	fullTimeEmployee: boolean;
+	yearsOfExperience?: number ;
+	location: string;
+	[key: string]: any;
 }
-const student1: student = {
-	firstName: "Melvyn",
-	lastName: "l'idiot",
-	age: 22,
-	location: "Marseille"
-};
-const student2: student = {
-	firstName: "Clément",
-	lastName: "lefou",
-	age: 22,
-	location: "Laval"
+
+const teacher3: teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
 };
 
-const studentsList: student[] = [student1, student2];
-
-const table = document.createElement("table");
-document.body.appendChild(table);
-
-studentsList.forEach((student) => {
-	const row = table.insertRow();
-	row.insertCell().textContent = student.firstName
-	row.insertCell().textContent = student.location
-});
+console.log(teacher3);

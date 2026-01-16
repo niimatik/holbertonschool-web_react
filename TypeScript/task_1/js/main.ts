34b10b17a725b1796fda9d8a9cb2ca81876d8579
niimatik <niimatik.gg@gmail.com>
2026-01-16 10:39:47 +0100

    add: added the task 3 part

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -1,3 +1,4 @@
+// Task 1
 interface teacher {
 	firstName: string;
 	lastName: string;
@@ -7,6 +8,16 @@ interface teacher {
 	[key: string]: any;
 }
 
+// Task 2
 interface Directors extends teacher {
 	numberOfReports: number
+}
+
+// Task 3
+interface printTeacherFunction{
+	(firstName: string, lastName: string): string
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+	return `${firstName.charAt(0)}. ${lastName}`;
 }
